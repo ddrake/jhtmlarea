8fe813630a7dfcfba1e325dcbebdfbb30042bedc
dow <dowdrake@msn.com>
2013-08-14 08:36:37 -0700

    trying to fix issue -- images not included in manifest in precompile

--- lib/jhtmlarea/engine.rb
+++ lib/jhtmlarea/engine.rb
@@ -2,6 +2,10 @@ module Jhtmlarea
   if defined?(::Rails) and Gem::Requirement.new('>= 3.1').satisfied_by?(Gem::Version.new ::Rails.version)
     module Rails
       class Engine < ::Rails::Engine
+        initializer "jhtmlarea.assets.precompile" do |app|
+          app.config.assets.precompile += 
+            %w(jHtmlArea.png jHtmlArea_Toolbar_Group_BG.png jHtmlArea_Toolbar_Group__Btn_Select_BG.png)
+        end
       end
     end
   end
--- lib/jhtmlarea/version.rb
+++ lib/jhtmlarea/version.rb
@@ -1,3 +1,3 @@
 module Jhtmlarea
-  VERSION = "0.0.7"
+  VERSION = "0.0.8"
 end
